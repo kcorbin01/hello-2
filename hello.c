c44980b2728d3344c620c47f19da7c5ad6fce9c9
Wes9504 <111804037+Wes9504@users.noreply.github.com>
2022-08-29 21:46:48 +0000

    added my name and major to partners file

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv) {
   printf("Hello, I'm Konner!\n");
   printf("Major is Computer Engineering!\n")
 
-  
+  printf("Hello, My name is Wesley Buryanek!\n");
+  printf("My Major is Computer Science!\n");
+
   return 0;
 }
