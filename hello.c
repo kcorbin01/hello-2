07ddb3ec62430d92c3682e23ed490923f90205f8
kcorbin01 <kcorbin01@huskers.unl.edu>
2022-08-29 20:26:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello, I'm Konner!\n");
+  printf("Major is Computer Engineering!\n")
+
+  
+  return 0;
+}
